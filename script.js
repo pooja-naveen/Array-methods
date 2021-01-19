9cb56b1c52e0e3e8b362a4e943bb0c895e740a46
pooja-naveen <pooja.naveen9119@gmail.com>
2021-01-19 22:57:16 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,6 @@
+const main = document.getElementById("main");
+const addUserBtn = document.getElementById("add-user");
+const doubleBtn = document.getElementById("double");
+const showMillionariesBtn = document.getElementById("show-millionaries");
+const sortBtn = document.getElementById("sort");
+const calculateWealthBtn = document.getElementById("calculate-wealth");
